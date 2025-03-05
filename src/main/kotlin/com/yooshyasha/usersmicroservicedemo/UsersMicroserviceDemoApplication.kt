0401a2e194179abf4111e3e12e43b68a8fb17839
Yooshyasha <mejzikrejzi66@gmail.com>
2025-03-05 19:52:36 +0500

    Initial commit

---
+++ src/main/kotlin/com/yooshyasha/usersmicroservicedemo/UsersMicroserviceDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.yooshyasha.usersmicroservicedemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UsersMicroserviceDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<UsersMicroserviceDemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/yooshyasha/usersmicroservicedemo/UsersMicroserviceDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yooshyasha.usersmicroservicedemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UsersMicroserviceDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
